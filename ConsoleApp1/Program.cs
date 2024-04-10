3db0cb0bed017055d303577512aa9383b9dc06a8
yomakaineru <141700115+yomakaineru@users.noreply.github.com>
2024-04-10 13:52:36 +0900

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Hello
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 12; i++)
+        {
+            for (int t = 1; t <= 12; t++)
+            {
+                System.Console.Write($"{i * t,4:d} ");
+            }
+            System.Console.Write("\r\n");
+        }
+    }
+}
